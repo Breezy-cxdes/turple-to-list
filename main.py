6c6c8d6bfbc1d4b1fd436dfc7e53dcb9b28c1ea5
Breezy-cxdes <snowbreezy20@gmail.com>
2024-09-11 17:52:24 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Create a tuple with personal details
+personal_details_tuple = ("Breezy", "Zondi", 19, 6.4, 70, "Mathematics")
+
+# Convert the tuple into a list
+personal_details_list = list(personal_details_tuple)
+
+# Display the list
+print("Personal details as a list:")
+print(personal_details_list)
